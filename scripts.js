a2f977227312a83a8990330abb567e74b4a62cd2
ckieferi <git@christinekiefer.de>
2018-12-03 11:42:12 +0100

    delete unused images and pages

--- scripts.js
+++ scripts.js
@@ -1,14 +1,3 @@
-
-// Get the modal
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
 /* fade in the next section when scrolling */
 
 function isElementInViewport(element) {
